c8d340bbdf9be805748b46312ee1b65a07a5cbea
mayara.barbosa <mayara.barbosa@LAD1016.cwbdis.pucpr.br>
2017-05-05 18:54:57 -0300

    projeto criado e método que envia mensagens comsleep de 10s

---
+++ ChristianAlgorithm/src/christianalgorithm/Servidor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package christianalgorithm;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ *
+ * @author mayara.barbosa
+ */
+public class Servidor {
+    
+    
+    
+    
+    
+    public void enviaMsg( DataOutputStream out, String msg) throws IOException, InterruptedException{
+        Random rand= new Random();
+        Thread.sleep(rand.nextInt(1000));
+        out.writeUTF(msg);
+    }
+}
