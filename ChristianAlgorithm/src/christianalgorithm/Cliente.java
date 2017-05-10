182eaeba533dcf3c72c844c87893ba92ab49c4cc
mayara.barbosa <>
2017-05-09 22:41:23 -0300

    falta fazer a conta do T no algoritmo e recalcular o horario da maquina

--- ChristianAlgorithm/src/christianalgorithm/Cliente.java
+++ ChristianAlgorithm/src/christianalgorithm/Cliente.java
@@ -5,8 +5,16 @@
  */
 package christianalgorithm;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -15,7 +23,28 @@ import java.util.Random;
  */
 public class Cliente {
     
+    public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException{
     
+        Cliente cli = new Cliente();
+        Socket clientSocket = new Socket(InetAddress.getLocalHost(), 5555);
+        DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader buf = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        String clientTime = "21:49:22";
+        final long t0 = System.nanoTime();
+        
+        cli.enviaMsg(out, "quero o horario certo");
+        long rightTime = Long.parseLong(in.readUTF());
+        long i = in.readLong();
+        final long t1 = System.nanoTime();
+        long d = t1 - t0 - i;
+        
+        long T = rightTime +  d;
+        System.out.println(T);
+   
+    
+    }
     public void enviaMsg( DataOutputStream out, String msg) throws IOException, InterruptedException{
         Random rand= new Random();
         Thread.sleep(rand.nextInt(1000));
--- ChristianAlgorithm/src/christianalgorithm/Servidor.java
+++ ChristianAlgorithm/src/christianalgorithm/Servidor.java
@@ -6,12 +6,17 @@
 package christianalgorithm;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -20,23 +25,36 @@ import java.util.Random;
  */
 public class Servidor {
     
-    public static void main (String [] args) throws IOException{
+    public static void main (String [] args) throws IOException, InterruptedException{
         //horario fixo para cliente
         //pergunta pro server
         ServerSocket serverSocket = new ServerSocket(5555);
-        
+        Servidor serv = new Servidor();
+         
         while(true){
             Socket socket = serverSocket.accept();
             BufferedReader buf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream()); 
             
+            final long durationI;
+            final long startTime = System.nanoTime();
+            String msg = in.readUTF(); 
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+            Date date = new Date();
+            String serverTime = dateFormat.format(date);
+            final long endTime = System.nanoTime();
+            durationI = endTime - startTime;
+                
+            serv.enviaMsg(out, serverTime, durationI);
         }
     
     
     }
-    public void enviaMsg( DataOutputStream out, String msg) throws IOException, InterruptedException{
+    public void enviaMsg( DataOutputStream out, String msg, long i) throws IOException, InterruptedException{
         Random rand= new Random();
         Thread.sleep(rand.nextInt(1000));
         out.writeUTF(msg);
+        out.writeLong(i);
     }
 }
